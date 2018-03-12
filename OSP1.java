19b96064ffb766af994baa6c0543ea2559c67a32
Dephinain <36282674+Dephinain@users.noreply.github.com>
2018-03-12 01:29:04 -0500

    updated and fixed for job deficit, still needs comments

--- OSP1.java
+++ OSP1.java
@@ -1,15 +1,30 @@
+/*
+Name: Myles Brown
+Course Number: CS 4332
+Assignment Title: Operating Systems Project - Phase 1
+Date: 2/26/2018
+Global Variables: J_SCHED sched - this is the job scheduler class that properly reads, sorts, and appends relevant information to the job based upon its ID, memory need, and class.
+				  jobsProcessed - Counts how many jobs have been processed by the program
+				  cpuJobCount  - Counts how many cpu-based jobs have been processed
+				  ioJobCount - Counts how many io-based jobs have been processed
+				  balancedJobCount - Counts how many io/cpu balanced jobs have been processed
+				  totalTurnaround - Gets the total turnaround time for all processed jobs
+				  totalWaitTime - Gets the total wait time for all processed jobs
+				  rejectCount - Counts how many jobs have been rejected due to being either empty jobs (0) or not meeting the memory requirements
+				  
+*/
+
 import java.io.*;
 import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.lang.*;
 
-//Note: All operations need to take place within 'while' loop reading from the file - handles everything one line at a time.
 public class OSP1
 {
 	public static J_SCHED sched = new J_SCHED();
 	public static int jobsProcessed;
-	public static int cpuJobCount, ioJobCount, balancedJobCount, totalTurnaround, totalWaitTime;
+	public static int cpuJobCount, ioJobCount, balancedJobCount, totalTurnaround, totalWaitTime, rejectCount, totalProcess;
 	
 	/*Job termination function. Documents and outputs job termination statistics, and purges the appropriate queues upon termination.*/
 	public static void J_TERM(String finishedJob)
@@ -19,8 +34,8 @@ public class OSP1
 		long tat_time, wait_time;
 		String[] term_tokens;
 		term_tokens = finishedJob.split("\\s+");
-		tat_time = term_time - Long.parseLong(term_tokens[5]);
-		wait_time = term_time - Long.parseLong(term_tokens[4]) - Long.parseLong(term_tokens[4]);
+		tat_time = term_time - Long.parseLong(term_tokens[4]) - Long.parseLong(term_tokens[5]);
+		wait_time = term_time - Long.parseLong(term_tokens[5]);
 		if(Integer.parseInt(term_tokens[2]) == 1)
 			cpuJobCount++;
 		else if(Integer.parseInt(term_tokens[2]) == 2)
@@ -33,7 +48,7 @@ public class OSP1
 		System.out.println("Class of Job: " + term_tokens[2]);
 		System.out.println("Time job was submitted: " + term_tokens[5] + " milliseconds.");
 		System.out.println("Time job was loaded to ready queue: " + term_tokens[6] + " milliseconds.");
-		System.out.println("Time job was terminated: " + term_time);
+		System.out.println("Time job was terminated at: " + term_time + " milliseconds.");
 		System.out.println("Time job was spent processing: " + term_tokens[4] + " milliseconds.");
 		System.out.println("Turnaround time: " + tat_time + " milliseconds.");
 		System.out.println("Waiting time: " + wait_time + " milliseconds.");
@@ -49,7 +64,7 @@ public class OSP1
 		dispatch_tokens = arrivingJob.split("\\s+");
 		try
 		{
-			Thread.sleep(Integer.parseInt(dispatch_tokens[4]));
+			totalProcess += Integer.parseInt(dispatch_tokens[4]);
 			J_TERM(arrivingJob);
 		}
 		catch(Exception ex)
@@ -60,11 +75,10 @@ public class OSP1
 	
 	public static void main(String[] args)
 	{
-		//File file = new File("C:\\Users\\Larry\\Desktop\\18Sp-jobs"); Line used for CSX reading the file, other line is for testing on local machines
+		//File file = new File("//home//opsys//OS-I//18sp-jobs"); Line used for CSX reading the file, other line is for testing on local machines
 		File file = new File("18sp-jobs");
 		String line;
 		String[] tokens;
-		int count = 0, queueCount = 0;
 		final int qCONSTRAINT = 26;
 		BlockingQueue<String> readyQueue = new ArrayBlockingQueue<String>(qCONSTRAINT);
 		BlockingQueue<String> disk = new ArrayBlockingQueue<String>(300);
@@ -87,7 +101,7 @@ public class OSP1
 					{
 						if(disk.size() == 300) //If disk is full, proceeds to empty the ready queue to offload jobs from disk
 						{
-							System.out.println("THIS DISK IS ONE THICC BIH");
+							//System.out.println("THIS DISK IS ONE THICC BIH");
 							for(int k = 0; k < readyQueue.size(); k++)
 							{
 								J_DISPATCH(readyQueue.take());
@@ -109,8 +123,18 @@ public class OSP1
 							}
 							continue;
 						}
-						disk.add(line);
-						line = disk.take();
+						String[] check_tokens;
+						check_tokens = line.split("\\s+");
+						if(sched.sizeCheck(Integer.parseInt(check_tokens[3])) == false)
+						{
+							rejectCount++;
+							continue;
+						}
+						else
+						{
+							disk.add(line);
+							line = disk.take();
+						}
 					}
 					
 					if(readyQueue.size() == qCONSTRAINT) //Checks if the ready queue is full. If it is, calls J_DISPATCH to run.
@@ -127,7 +151,6 @@ public class OSP1
 							{
 								if(sched.idCheck(disk.element()))
 								{
-									System.out.println("Henlo");
 									disk.take();
 								}
 								else
@@ -166,15 +189,37 @@ public class OSP1
 						String[] check_tokens;
 						check_tokens = line.split("\\s+");
 						if(sched.sizeCheck(Integer.parseInt(check_tokens[3])) == false)
+						{
+							rejectCount++;
 							continue;
+						}
 						else
 						{
-							//System.out.println("The job takes up this much memory: " + Integer.parseInt(check_tokens[3]));
 							disk.add(line);
 						}
 					}
 			}
 			bufferedReader.close();
+			
+			while(disk.isEmpty() == false) //Empties disk after job input has ceased
+			{
+				String arrival, loaded;
+				StringBuffer appendArrivalTime = new StringBuffer(disk.take());
+				appendArrivalTime.append("	" + date.getTime());
+				arrival = appendArrivalTime.toString();
+				
+				if(readyQueue.size() == qCONSTRAINT)
+				{
+					J_DISPATCH(readyQueue.take());
+				}
+				else
+				{
+					StringBuffer appendLoadTime = new StringBuffer(arrival);
+					appendLoadTime.append("	" + date.getTime());
+					loaded = appendLoadTime.toString();
+					readyQueue.add(loaded);
+				}
+			}
 			
 			while(readyQueue.isEmpty() == false) //Empties ready queue after job input has ceased
 			{
@@ -194,14 +239,15 @@ public class OSP1
 		{
 			ex.printStackTrace(System.out);
 		}
-		System.out.println("\nThe number of items entered into the disk is: " + count);
+		System.out.println("\nThe number of items entered into the disk is: " + disk.size());
 		System.out.println("The number of items entered into the ready queue is " + readyQueue.size());
-		System.out.println("Number of jobs processed: " + jobsProcessed);
+		System.out.println("\nNumber of jobs processed: " + jobsProcessed);
 		System.out.println("Number of CPU-bound jobs: " + cpuJobCount);
 		System.out.println("Number of Balanced jobs: " + balancedJobCount);
 		System.out.println("Number of IO-bound jobs: " + ioJobCount);
 		System.out.println("Average turnaround time: " + (totalTurnaround/jobsProcessed) + " milliseconds.");
 		System.out.println("Average wait time: " + Math.abs(totalWaitTime/jobsProcessed) + " milliseconds.");
-		System.out.println("Time of program termination: " + date.getTime());
+		System.out.println("Number of rejected jobs: " + rejectCount);
+		System.out.println("Total processing time of CPU clock: " + totalProcess);
 	}
 }
