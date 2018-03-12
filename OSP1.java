955181ff96523c14df78e31ade5463a54c468ded
Dephinain <36282674+Dephinain@users.noreply.github.com>
2018-03-12 02:40:56 -0500

    Hopefully it stops reading 0 jobs, still workin out kinks

--- OSP1.java
+++ OSP1.java
@@ -11,6 +11,11 @@ Global Variables: J_SCHED sched - this is the job scheduler class that properly 
 				  totalTurnaround - Gets the total turnaround time for all processed jobs
 				  totalWaitTime - Gets the total wait time for all processed jobs
 				  rejectCount - Counts how many jobs have been rejected due to being either empty jobs (0) or not meeting the memory requirements
+				  totalProcess - Counts how many processes have been run
+				  qCONSTRAINT - The hardcap on memory availability
+				  
+File description: Main protocol for the scheduler, takes input from the incoming job file and calls appropriate methods and functions (J_SCHED, J_DISPATCH, J_TERM) according to input
+Possible improvements: <to be added, but lord almighty is this definitely not perfect>
 				  
 */
 
@@ -25,47 +30,59 @@ public class OSP1
 	public static J_SCHED sched = new J_SCHED();
 	public static int jobsProcessed;
 	public static int cpuJobCount, ioJobCount, balancedJobCount, totalTurnaround, totalWaitTime, rejectCount, totalProcess;
+	public static final int qCONSTRAINT = 26;
 	
 	/*Job termination function. Documents and outputs job termination statistics, and purges the appropriate queues upon termination.*/
 	public static void J_TERM(String finishedJob)
 	{
-		Date time = new Date();
-		long term_time = time.getTime();
-		long tat_time, wait_time;
-		String[] term_tokens;
-		term_tokens = finishedJob.split("\\s+");
-		tat_time = term_time - Long.parseLong(term_tokens[4]) - Long.parseLong(term_tokens[5]);
-		wait_time = term_time - Long.parseLong(term_tokens[5]);
-		if(Integer.parseInt(term_tokens[2]) == 1)
-			cpuJobCount++;
-		else if(Integer.parseInt(term_tokens[2]) == 2)
-			balancedJobCount++;
-		else if(Integer.parseInt(term_tokens[2]) == 3)
-			ioJobCount++;
 		
-		System.out.println("\nJob stats: ");
-		System.out.println("Job ID: " + term_tokens[1]);
-		System.out.println("Class of Job: " + term_tokens[2]);
-		System.out.println("Time job was submitted: " + term_tokens[5] + " milliseconds.");
-		System.out.println("Time job was loaded to ready queue: " + term_tokens[6] + " milliseconds.");
-		System.out.println("Time job was terminated at: " + term_time + " milliseconds.");
-		System.out.println("Time job was spent processing: " + term_tokens[4] + " milliseconds.");
-		System.out.println("Turnaround time: " + tat_time + " milliseconds.");
-		System.out.println("Waiting time: " + wait_time + " milliseconds.");
-		sched.releaseMemory(finishedJob);
-		totalTurnaround += tat_time;
-		totalWaitTime += wait_time;
-		jobsProcessed++;
+		Date time = new Date(); //Gets the time of execution/termination
+		long term_time = time.getTime(); //Termination time
+		long tat_time, wait_time; //Turnaround time and wait time
+		String[] term_tokens; //Array to hold to-be tokenized job string
+		term_tokens = finishedJob.split("\\s+"); //Tokenizes terminating job string
+		if(Integer.parseInt(term_tokens[1]) == 0)
+		{
+			System.out.println("Bad value got through.");
+		}
+		else
+		{
+			tat_time = term_time - Long.parseLong(term_tokens[4]) - Long.parseLong(term_tokens[5]);
+			wait_time = term_time - Long.parseLong(term_tokens[5]);
+			if(Integer.parseInt(term_tokens[2]) == 1)
+				cpuJobCount++;
+			else if(Integer.parseInt(term_tokens[2]) == 2)
+				balancedJobCount++;
+			else if(Integer.parseInt(term_tokens[2]) == 3)
+				ioJobCount++;
+		
+			//Entire following brick outputs individual job statistics
+			System.out.println("\nJob stats: ");
+			System.out.println("Job ID: " + term_tokens[1]);
+			System.out.println("Class of Job: " + term_tokens[2]);
+			System.out.println("Time job was submitted: " + term_tokens[5] + " milliseconds.");
+			System.out.println("Time job was loaded to ready queue: " + term_tokens[6] + " milliseconds.");
+			System.out.println("Time job was terminated at: " + term_time + " milliseconds.");
+			System.out.println("Time job was spent processing: " + term_tokens[4] + " milliseconds.");
+			System.out.println("Turnaround time: " + tat_time + " milliseconds.");
+			System.out.println("Waiting time: " + wait_time + " milliseconds.");
+			sched.releaseMemory(finishedJob); //Inputs job string to release memory after execution
+			totalTurnaround += tat_time;
+			totalWaitTime += wait_time;
+			jobsProcessed++;
+		}
+		
 	}
 	
+	//Executes/processes incoming job from ready queue
 	public static void J_DISPATCH(String arrivingJob)
 	{
-		String[] dispatch_tokens;
-		dispatch_tokens = arrivingJob.split("\\s+");
+		String[] dispatch_tokens; //Array to hold to-be tokenized
+		dispatch_tokens = arrivingJob.split("\\s+"); //Tokenizes executing job string
 		try
 		{
 			totalProcess += Integer.parseInt(dispatch_tokens[4]);
-			J_TERM(arrivingJob);
+			J_TERM(arrivingJob); //Calls after job 'execution' to output termination stats
 		}
 		catch(Exception ex)
 		{
@@ -76,44 +93,41 @@ public class OSP1
 	public static void main(String[] args)
 	{
 		//File file = new File("//home//opsys//OS-I//18sp-jobs"); Line used for CSX reading the file, other line is for testing on local machines
-		File file = new File("18sp-jobs");
-		String line;
-		String[] tokens;
-		final int qCONSTRAINT = 26;
-		BlockingQueue<String> readyQueue = new ArrayBlockingQueue<String>(qCONSTRAINT);
-		BlockingQueue<String> disk = new ArrayBlockingQueue<String>(300);
-		Date date = new Date();
+		File file = new File("18sp-jobs"); //Incoming job file
+		String line; //Line that holds incoming job to be checked
+		BlockingQueue<String> readyQueue = new ArrayBlockingQueue<String>(qCONSTRAINT); //Ready queue initialized with the constraint
+		BlockingQueue<String> disk = new ArrayBlockingQueue<String>(300); //Disk initialized with the constraint given by the assignment documentation
+		Date date = new Date(); //Used to get the time at certain key moments in scheduler
 		
 		
 		//This chunk runs everything. J_SCHED runs first after the initial line is read in and sent to it.
 		try 
 		{
-			FileReader fileReader = new FileReader(file);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			FileReader fileReader = new FileReader(file); //Preps input stream of file
+			BufferedReader bufferedReader = new BufferedReader(fileReader); //Buffers input stream for main program loop
 			
 			while((line = bufferedReader.readLine()) != null)
 			{
 				
-					StringBuffer appendArrivalTime = new StringBuffer(line);
+					StringBuffer appendArrivalTime = new StringBuffer(line); //StringBuffer used to append 'job queue arrival time' to the incoming job string
 					appendArrivalTime.append("	" + date.getTime());
 					line = appendArrivalTime.toString(); //appends arrival time to incoming job
 					if(disk.isEmpty() == false)
 					{
 						if(disk.size() == 300) //If disk is full, proceeds to empty the ready queue to offload jobs from disk
 						{
-							//System.out.println("THIS DISK IS ONE THICC BIH");
-							for(int k = 0; k < readyQueue.size(); k++)
+							for(int k = 0; k < readyQueue.size(); k++) //Jobs from ready queue ran to offload jobs from disk
 							{
 								J_DISPATCH(readyQueue.take());
 							}
 							
-							for(int j = 0; j < qCONSTRAINT; j++)
+							for(int j = 0; j < qCONSTRAINT; j++) //Adds jobs from disk to ready queue
 							{
 								if(readyQueue.size() == qCONSTRAINT)
 									break;
 								else
 								{
-									StringBuffer appendLoadTime = new StringBuffer(disk.element());
+									StringBuffer appendLoadTime = new StringBuffer(disk.element()); //Another string buffer used to append 'ready queue load time' to incoming job string
 									String tempItem;
 									appendLoadTime.append("	" + date.getTime());
 									tempItem = appendLoadTime.toString();
@@ -123,9 +137,9 @@ public class OSP1
 							}
 							continue;
 						}
-						String[] check_tokens;
-						check_tokens = line.split("\\s+");
-						if(sched.sizeCheck(Integer.parseInt(check_tokens[3])) == false)
+						String[] check_tokens; //Array used to hold to-be tokenized job string to see if its memory is within proper constraints
+						check_tokens = line.split("\\s+"); //Tokenizes job string
+						if(sched.sizeCheck(Integer.parseInt(check_tokens[3])) == false) //Checks if the memory requested by the incoming job is within available parameters
 						{
 							rejectCount++;
 							continue;
@@ -147,7 +161,7 @@ public class OSP1
 					{
 						for(int j = 0; j < qCONSTRAINT; j++)
 						{
-							if(sched.aquireMemoryCheck(disk.element()))
+							if(sched.aquireMemoryCheck(disk.element())) //Pops 0 job off stack
 							{
 								if(sched.idCheck(disk.element()))
 								{
